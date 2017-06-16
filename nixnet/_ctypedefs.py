1e797119cebd2086c888346ccba08c26c4a7c9b2
Ed Page <eopage@byu.net>
2017-06-16 16:24:08 -0500

    feat: Low-level (ctypes) wrapper around nixnet.dll
    A custom library class wraps ctypes
    - To allow delay loading to give an opportunity for unity tests to run
    - For a mockable interface for testing
    - For an interface we can attach types to for mypyp.
    
    To do this, a `_ctypedefs` is created that matches the types represented
    in the C API for NI-XNET.  This makes it easier to compare the C header
    file with `_cfuncs` and allows the opportunity for using mypy to do
    stricter type checking within the module.
    
    Assuming nidaqmx-python as a skeleton, this deviates for the following
    reasons
    - Import functions are decouple from platform lookup for easier debugging
      of import problems and to allow testing of `_import_unsupported`.
    - Only loading `cdll` because XNET doesn't use StdCall.
    - XNET doesn't use different handle types for different bitness, like
      DAQmx.
    - Track exception message in exceptions rather than callers.
    - An equivelant of `CtypesBytesString` isn't used yet because we need to
      work out how we should handle that for NI-XNET.
    - No `c_bool32` because NI-XNET doesn't use a bool type in its function
      declarations.  It is used for properties but we'll need to see when we
      get there if there would be value in creating something like this.
    - A class that encapsulates the c-functions
      - Allow mocking out the c-functions.
      - Allow adding mypy annotations at a later point for extra validation.
      - Centralize c-function knowledge for easier comparing to the header
        file.
      - Abstracted so we can add "caching" of the c-function declarations at a
        later point if we deem it useful.  Right now we follow DAQ's approach
        of on-call initialization to keep it simple for unsupported functions.
    
    Fixes not yet in daqmx-python
    - Raise an unsupported platform error rather than some obscure error with
      `None`
    
    Fixed #10

---
+++ nixnet/_ctypedefs.py
@@ -0,0 +1,126 @@
+"""
+Definition of types for calling into NI-XNET.
+
+Distinct types (rather than aliases) are used to allow more accurate type
+checking.
+"""
+
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import ctypes
+
+
+class char(ctypes.c_char):  # NOQA: N801
+    pass
+
+
+class i8(ctypes.c_byte):  # NOQA: N801
+    pass
+
+
+class i16(ctypes.c_short):  # NOQA: N801
+    pass
+
+
+class i32(ctypes.c_int):  # NOQA: N801
+    pass
+
+
+class i64(ctypes.c_longlong):  # NOQA: N801
+    pass
+
+
+class u8(ctypes.c_ubyte):  # NOQA: N801
+    pass
+
+
+class u16(ctypes.c_ushort):  # NOQA: N801
+    pass
+
+
+class u32(ctypes.c_uint):  # NOQA: N801
+    pass
+
+
+class u64(ctypes.c_ulonglong):  # NOQA: N801
+    pass
+
+
+class f32(ctypes.c_float):  # NOQA: N801
+    pass
+
+
+class f64(ctypes.c_double):  # NOQA: N801
+    pass
+
+
+class nxVoidPtr(ctypes.c_void_p):  # NOQA: N801
+    pass
+
+
+class nxSessionRef_t(u32):  # NOQA: N801
+    pass
+
+
+class nxDatabaseRef_t(u32):  # NOQA: N801
+    pass
+
+
+class nxStatus_t(i32):  # NOQA: N801
+    pass
+
+
+class nxTimeStamp_t(u64):  # NOQA: N801
+    pass
+
+
+class nxFlexRayStats_t(ctypes.Structure):  # NOQA: N801
+    _fields_ = [
+        ("NumSyntaxErrorChA", u32),
+        ("NumSyntaxErrorChB", u32),
+        ("NumContextErrorChB", u32),
+        ("NumContextErrorChB", u32),
+        ("NumSlotBoundaryViolationChB", u32),
+        ("NumSlotBoundaryViolationChB", u32)]
+
+
+class nxJ1939CommState_t(ctypes.Structure):  # NOQA: N801
+    _fields_ = [
+        ("PGN", u32),
+        ("SourceAddress", u8),
+        ("DestinationAddress", u8),
+        ("TransmitError", u8),
+        ("ReceiveError", u8),
+        ("Reserved1", u32),
+        ("Reserved2", u32)]
+
+
+class nxFrameFixed_t(ctypes.Structure):  # NOQA: N801
+    _fields_ = [
+        ("Timestamp", nxTimeStamp_t),
+        ("Identifier", u32),
+        ("Type", u8),
+        ("Flags", u8),
+        ("Info", u8)]
+
+
+class nxFrameCAN_t(nxFrameFixed_t):  # NOQA: N801
+    _fields_ = [
+        ("PayloadLength", u8),
+        ("Payload", u8 * 8)]
+
+
+class nxFrameLIN_t(nxFrameFixed_t):  # NOQA: N801
+    _fields_ = [
+        ("PayloadLength", u8),
+        ("Payload", u8 * 8)]
+
+
+class nxFrameVar_t(nxFrameFixed_t):  # NOQA: N801
+    _fields_ = [
+        ("PayloadLength", u8),
+        ("Payload", u8 * 8)]
---
+++ nixnet/_lib.py
@@ -0,0 +1,71 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+
+import ctypes
+import sys
+
+
+class Error(Exception):
+    pass
+
+
+class PlatformUnsupportedError(Error):
+
+    def __init__(self, platform):
+        message = '{0} is unsupported by this package.'.format(platform)
+        Error.__init__(self, message, platform)
+
+
+class XnetNotFoundError(Error):
+
+    def __init__(self, *args):
+        message = (
+            'Could not find an installation of NI-XNET. Please '
+            'ensure that NI-XNET is installed on this machine or '
+            'contact National Instruments for support.')
+        Error.__init__(self, message, *args)
+
+
+class XnetFunctionNotSupportedError(Error):
+
+    def __init__(self, function):
+        message = (
+            'The NI-XNET function "{0}" is not supported in this '
+            'version of NI-XNET. Visit ni.com/downloads to upgrade your '
+            'version of NI-XNET.'.format(function))
+        Error.__init__(self, message, function)
+
+
+class XnetLibrary(object):
+    """Proxy Library to consolidate nixnet-specific logic."""
+
+    def __init__(self, library):
+        self._library = library
+
+    def __getattr__(self, function):
+        try:
+            return getattr(self._library, function)
+        except AttributeError:
+            raise XnetFunctionNotSupportedError(function)
+
+
+def _import_win_lib(self):
+    lib_name = "nixnet"
+    try:
+        cdll = ctypes.cdll.LoadLibrary(lib_name)
+    except OSError:
+        raise XnetNotFoundError()
+    return XnetLibrary(cdll)
+
+
+def _import_unsupported(self):
+    raise PlatformUnsupportedError(sys.platform)
+
+
+if sys.platform.startswith('win') or sys.platform.startswith('cli'):
+    import_lib = _import_win_lib
+else:
+    import_lib = _import_unsupported
