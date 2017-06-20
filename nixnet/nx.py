c54bd47934b608fc3d979d39fab38d8e33f3cf6c
Ajay Jashnani <ajay.jashnani@ni.com>
2017-06-20 11:41:45 -0500

    feat(API): Specced out for can_frame_stream_input
    This is a preliminary look at the API sufficient to write the
    can_frame_stream_input example.  This serves as a stepping stone towards
    completeing #22 / #23.
    
    Some areas of risk
    - One `Session` type or one per-mode?
    - Which module parts of the API go into?
    - Impracticalities of implementing LV-like high level API on top of the C
      API (like READ_ALL_AVAILABLE).
    
    Closes #27

--- nixnet/nx.py
+++ nixnet/nx.py
@@ -3,15 +3,87 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+import warnings
 
-def create_session(
-        database_name,
-        cluster_name,
-        list,
-        interface,
-        mode,
-        session_ref):
-    raise NotImplementedError("Placeholder")
+from nixnet import constants
+from nixnet import errors
+
+
+class Session(object):
+
+    def __init__(
+            self,
+            database_name,
+            cluster_name,
+            list,
+            interface,
+            mode):
+        "http://zone.ni.com/reference/en-XX/help/372841N-01/nixnet/nxcreatesession/"
+        # What about http://zone.ni.com/reference/en-XX/help/372841N-01/nixnet/nxcreatesessionbyref/
+        # Store sessionRef as a member variable
+        raise NotImplementedError("Placeholder")
+
+    def __del__(self):
+        if self._handle is not None:
+            warnings.warn(
+                'Session was not explicitly closed before it was destructed. '
+                'Resources on the device may still be reserved.',
+                errors.XnetResourceWarning)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self):
+        self.close()
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self._handle == other._handle
+        return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __hash__(self):
+        return hash(self._handle)
+
+    def __repr__(self):
+        return 'Session(handle={0})'.format(self._handle)
+
+    def close(self):
+        "http://zone.ni.com/reference/en-XX/help/372841N-01/nixnet/nxclear/"
+        if self._handle is None:
+            warnings.warn(
+                'Attempting to close NI-XNET session but session was already '
+                'closed', errors.XnetResourceWarning)
+            return
+
+        raise NotImplementedError("Placeholder")
+
+        self._handle = None
+
+    def read_frame(
+            self,
+            number_to_read=constants.READ_ALL_AVAILABLE,
+            timeout=constants.TIMEOUT_NONE):
+        """Read frames.
+
+        Valid modes
+        - Frame Input Stream Mode
+        - Frame Input Queued Mode
+        - Frame Input Single-Point Mode
+        Frame: one or more
+        http://zone.ni.com/reference/en-XX/help/372841N-01/nixnet/nxreadframe/
+        """
+        raise NotImplementedError("Placeholder")
+
+    @property
+    def intf_baud_rate(self):
+        raise NotImplementedError("Placeholder")
+
+    @intf_baud_rate.setter
+    def intf_baud_rate(self, value):
+        raise NotImplementedError("Placeholder")
 
 
 def create_session_by_ref(
@@ -20,29 +92,6 @@ def create_session_by_ref(
         interface,
         mode,
         session_ref):
-    raise NotImplementedError("Placeholder")
-
-
-def get_property(
-        session_ref,
-        property_id,
-        property_size,
-        property_value):
-    raise NotImplementedError("Placeholder")
-
-
-def get_property_size(
-        session_ref,
-        property_id,
-        property_size):
-    raise NotImplementedError("Placeholder")
-
-
-def set_property(
-        session_ref,
-        property_id,
-        property_size,
-        property_value):
     raise NotImplementedError("Placeholder")
 
 
@@ -69,15 +118,6 @@ def set_sub_property(
         property_id,
         property_size,
         property_value):
-    raise NotImplementedError("Placeholder")
-
-
-def read_frame(
-        session_ref,
-        buffer,
-        size_of_buffer,
-        timeout,
-        number_of_bytes_returned):
     raise NotImplementedError("Placeholder")
 
 
