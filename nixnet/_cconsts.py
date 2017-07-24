08e402552a33678976534404ad9fe6f417c35a7e
Ed Page <eopage@byu.net>
2017-07-24 07:28:29 -0500

    fix: Py2 string compat issues
    Fixes #116 and #127

--- nixnet/_cconsts.py
+++ nixnet/_cconsts.py
@@ -1,7 +1,6 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-from __future__ import unicode_literals
 
 
 NX_STATUS_QUALIFIER = (0x3FF63000)
