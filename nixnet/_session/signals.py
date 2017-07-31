a708f2502ad1c722175aeec5c524fbda2d2fa727
Ed Page <eopage@byu.net>
2017-07-31 08:49:59 -0500

    fix(API): Limit `resamp_rate` to only relevant modes.
    Not really counting this as a breaking change because the property
    would error in the other modes anyways.
    
    Fixes #121

--- nixnet/_session/signals.py
+++ nixnet/_session/signals.py
@@ -21,20 +21,6 @@ class Signals(collection.Collection):
     def _create_item(self, handle, index, name):
         return Signal(handle, index, name)
 
-    @property
-    def resamp_rate(self):
-        # type: () -> float
-        """float: Rate used to resample frame data to/from signal data in waveforms.
-
-        The units are in Hertz (samples per second).
-        """
-        return _props.get_session_resamp_rate(self._handle)
-
-    @resamp_rate.setter
-    def resamp_rate(self, value):
-        # type: (float) -> None
-        _props.set_session_resamp_rate(self._handle, value)
-
 
 class SinglePointInSignals(Signals):
     """Writeable signals in a session."""
@@ -79,6 +65,20 @@ class WaveformInSignals(Signals):
     def __repr__(self):
         return 'Session.WaveformInSignals(handle={0})'.format(self._handle)
 
+    @property
+    def resamp_rate(self):
+        # type: () -> float
+        """float: Rate used to resample frame data to/from signal data in waveforms.
+
+        The units are in Hertz (samples per second).
+        """
+        return _props.get_session_resamp_rate(self._handle)
+
+    @resamp_rate.setter
+    def resamp_rate(self, value):
+        # type: (float) -> None
+        _props.set_session_resamp_rate(self._handle, value)
+
     def read(
             self,
             num_values_per_signal,
@@ -121,6 +121,20 @@ class WaveformOutSignals(Signals):
 
     def __repr__(self):
         return 'Session.WaveformOutSignals(handle={0})'.format(self._handle)
+
+    @property
+    def resamp_rate(self):
+        # type: () -> float
+        """float: Rate used to resample frame data to/from signal data in waveforms.
+
+        The units are in Hertz (samples per second).
+        """
+        return _props.get_session_resamp_rate(self._handle)
+
+    @resamp_rate.setter
+    def resamp_rate(self, value):
+        # type: (float) -> None
+        _props.set_session_resamp_rate(self._handle, value)
 
     def write(
             self,
